c2d5a7c72236c3a31090387a9183ac08ad949a91
Aniruddha Pawar <aniruddhapawar@Aniruddhas-MacBook-Air.local>
2026-06-22 22:34:07 +0530

    Implement password encryption using BCrypt

--- src/main/java/com/ascenderp/service/UserService.java
+++ src/main/java/com/ascenderp/service/UserService.java
@@ -2,18 +2,27 @@ package com.ascenderp.service;
 
 import com.ascenderp.entity.User;
 import com.ascenderp.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(User user) {
+
+        user.setPassword(
+                passwordEncoder.encode(user.getPassword())
+        );
+
         return userRepository.save(user);
     }
 }
