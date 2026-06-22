023f1c9a46a2dc9cf5bad0235d6d852ee09d19d1
Aniruddha Pawar <aniruddhapawar@Aniruddhas-MacBook-Air.local>
2026-06-22 22:28:20 +0530

    Implement user registration service

---
+++ src/main/java/com/ascenderp/service/UserService.java
@@ -0,0 +1,19 @@
+package com.ascenderp.service;
+
+import com.ascenderp.entity.User;
+import com.ascenderp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+}
