f7ca756af7d7c30499c238ec1e635b94376687b0
Aniruddha Pawar <aniruddhapawar@Aniruddhas-MacBook-Air.local>
2026-06-22 23:07:03 +0530

    Added user registration REST API

---
+++ src/main/java/com/ascenderp/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.ascenderp.controller;
+
+import com.ascenderp.entity.User;
+import com.ascenderp.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
